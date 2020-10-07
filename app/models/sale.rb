60633d22e310ffcd884bd78e5ac867c05cb03bf5
tkivite-lipalater <tkivite@lipalater.com>
2020-10-07 08:55:13 +0300

    added sale model

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Sale < ApplicationRecord
+  validates :customer_phone_number,:customer_id_number, :buying_price,:approved_amount, :item,:item_type,:store, :pick_up_type presence: true
+  validates :external_id, uniqueness: true
 end
